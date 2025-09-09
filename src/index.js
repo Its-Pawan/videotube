4cdab3bce17df18aa2e6eb3d6c0b9110163a2ab8
Pawan Joshi <business.pawanjoshi@gmail.com>
2025-09-09 13:15:17 +0530

    add API error handling and response utilities; configure CORS and cookie parsing in the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js"
+import { app } from './app.js'
 
 // console.log(process.env)
+const port = process.env.PORT || 8000;
 connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`⚙️  Server is running at port: ${port}`)
+        })
+    })
+    .catch(err => console.log("Database connection failed !!!"))
